6c3a53a45c7ecb5ffc6a57cbc6d5a782156a3393
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-06-21 19:54:02 +0200

    Add link button to welcome message

--- norris/events/member_join.py
+++ norris/events/member_join.py
@@ -1,4 +1,5 @@
-from discord import Colour, Embed, Member
+from discord import Colour, Embed, Member, ButtonStyle, HTTPException, Forbidden
+from discord.ui import View, Button
 from sqlalchemy import insert
 
 from ..bot import Norris
@@ -17,12 +18,26 @@ async def on_member_join(norris: Norris, member: Member) -> None:
             .values(user_id=member.id, status=RegistrationStatus.UNREGISTERED),
         )
 
-        await norris.get_channel(norris.arrival_channel_id).send(
-            embed=Embed(
-                title="Registration",
-                description=f"Welcome to the **University of Nottingham Computer "
-                            f"Science** server, <@{member.id}>! Please check your "
-                            f"direct messages for instructions on how to continue.",
-                colour=Colour.blurple(),
-            ),
-        )
+        try:
+            await norris.get_channel(norris.arrival_channel_id).send(
+                embed=Embed(
+                    title="Registration",
+                    description=f"Welcome to the **University of Nottingham Computer "
+                                f"Science** server, <@{member.id}>! Please check your "
+                                f"direct messages for instructions on how to continue.",
+                    colour=Colour.blurple(),
+                ),
+                view=OpenDirectMessagesView(),
+            )
+        except (Forbidden, HTTPException):
+            pass
+
+
+class OpenDirectMessagesView(View):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Add link button to open DMs
+        self.add_item(Button(label="Open direct messages",
+                             style=ButtonStyle.link,
+                             url="https://discord.com/channels/@me"))

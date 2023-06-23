8389aaa6e7360f0f5885ea5a4d082ef3f66f2f7c
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-06-23 07:45:12 +0200

    Remove unused code

--- src/error.rs
+++ src/error.rs
@@ -6,14 +6,10 @@ use sqlx::Error as SqlError;
 
 use thiserror::Error;
 
-// use crate::model::DecodeRegistrationError;
-
 #[derive(Debug, Error)]
 pub enum BotError {
     #[error("discord error: {}", .0)]
     Discord(#[from] SerenityError),
     #[error("{}", .0)]
     Sql(#[from] SqlError),
-    // #[error("could not decode registration status ({})", .0)]
-    // RegistrationParse(#[from] DecodeRegistrationError),
 }

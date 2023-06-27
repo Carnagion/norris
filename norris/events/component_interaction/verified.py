cd576261188c79d539c8f8aca9a923761bb92fd0
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-06-27 09:35:51 +0200

    Fix interaction response

--- norris/events/component_interaction/verified.py
+++ norris/events/component_interaction/verified.py
@@ -4,11 +4,14 @@ from ...bot import Norris
 
 
 async def continue_clicked(interaction: Interaction, norris: Norris) -> None:
+    # TODO: this will be useful for later
+    await interaction.response.defer()
+
     # NOTE: I hate circular imports
     from ...responses import PronounsView, pronouns_embed
 
     # Ask the user their pronouns
-    await interaction.response.send(
+    await interaction.followup.send(
         embed=pronouns_embed(),
         view=PronounsView(norris),
     )

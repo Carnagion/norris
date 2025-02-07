68aa0db83db661ef7cb0b7eaa0a57d8e597cf4ad
sanamorii <contact@sanamorii.com>
2025-02-07 00:18:58 +0000

    Corrected docker-compose.yml

--- init.sql
+++ init.sql
@@ -6,10 +6,12 @@ CREATE TABLE `registrations` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
-CREATE TABLE `registrations` (
-  `user_id` bigint(20) unsigned NOT NULL,
-  `status` enum('UNREGISTERED','STARTED','NAME_ENTERED','KIND_FOUND','VERIFIED','PRONOUNS_PICKED','REGISTERED','FAILED') NOT NULL DEFAULT 'UNREGISTERED',
-  `name` varchar(1024) DEFAULT NULL,
-  `kind` enum('UNDERGRAD','POSTGRAD','MENTOR','SENIOR_MENTOR','HONORARY_MENTOR','FACULTY') DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+CREATE TABLE `users` (
+  `name` varchar(1024) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+  `kind` enum('UNDERGRAD','POSTGRAD','MENTOR','SENIOR_MENTOR','HONORARY_MENTOR','FACULTY') CHARACTER SET utf16 COLLATE utf16_unicode_ci DEFAULT NULL,
+  `registered_user_id` bigint(20) unsigned DEFAULT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `users_registrations_FK` (`registered_user_id`),
+  CONSTRAINT `users_registrations_FK` FOREIGN KEY (`registered_user_id`) REFERENCES `registrations` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=777 DEFAULT CHARSET=utf16 COLLATE=utf16_bin;

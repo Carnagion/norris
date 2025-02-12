e6e636cde877b371d37e6c6ff0883058b6a03410
sanamorii <contact@sanamorii.com>
2025-02-12 09:46:51 +0000

    update init.sql to use utf-8 collate

--- init.sql
+++ init.sql
@@ -4,14 +4,14 @@ CREATE TABLE `registrations` (
   `name` varchar(1024) DEFAULT NULL,
   `kind` enum('UNDERGRAD','POSTGRAD','MENTOR','SENIOR_MENTOR','HONORARY_MENTOR','FACULTY') DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE `users` (
-  `name` varchar(1024) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
-  `kind` enum('UNDERGRAD','POSTGRAD','MENTOR','SENIOR_MENTOR','HONORARY_MENTOR','FACULTY') CHARACTER SET utf16 COLLATE utf16_unicode_ci DEFAULT NULL,
+  `name` varchar(1024) NOT NULL,
+  `kind` enum('UNDERGRAD','POSTGRAD','MENTOR','SENIOR_MENTOR','HONORARY_MENTOR','FACULTY') DEFAULT NULL,
   `registered_user_id` bigint(20) unsigned DEFAULT NULL,
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `users_registrations_FK` (`registered_user_id`),
   CONSTRAINT `users_registrations_FK` FOREIGN KEY (`registered_user_id`) REFERENCES `registrations` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=777 DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
